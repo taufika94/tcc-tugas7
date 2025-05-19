1de5c02f1da16e5dfea94a2b7fe28048656839eb
taufika <taufikaa94@gmail.com>
2025-05-19 15:21:53 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 dotenv.config();
 app.use(cookieParser());
 app.use(cors({ credentials:true,origin:'http://localhost:3000' }));
-app.get("/", (req, res) => res.render("index"));
+app.set('view engine', 'ejs');
+app.get("/", (req, res) => res.json({ message: "API is running" }));
 app.use(UserRoute);
 app.use(NotesRoute);
 
